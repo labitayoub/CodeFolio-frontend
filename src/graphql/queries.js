cb388fed341aa5264ef128517495aea54930b018
ayoub.labite@gmail.com <ayoub.labite@gmail.com>
2025-11-09 01:53:04 +0100

    update the queries of graphql

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,68 +1,55 @@
-import { gql } from '@apollo/client';
-
-// Query pour récupérer l'utilisateur connecté
-export const GET_CURRENT_USER = gql`
-  query GetCurrentUser {
-    me {
-      id
-      nom
-      prenom
-      email
-      bio
-      createdAt
-    }
-  }
-`;
-
-// Query pour récupérer tous les projets
-export const GET_PROJECTS = gql`
-  query GetProjects {
-    projects {
-      id
-      title
-      description
-      technologies
-      createdAt
-    }
-  }
-`;
-
-// Query pour récupérer toutes les compétences
-export const GET_SKILLS = gql`
-  query GetSkills {
-    skills {
-      id
-      name
-      level
-      category
-    }
-  }
-`;
-
-// Query pour récupérer les expériences
-export const GET_EXPERIENCES = gql`
-  query GetExperiences {
-    experiences {
-      id
-      title
-      company
-      description
-      startDate
-      endDate
-    }
-  }
-`;
-
-// Query pour récupérer les formations
-export const GET_FORMATIONS = gql`
-  query GetFormations {
-    formations {
-      id
-      title
-      institution
-      description
-      startDate
-      endDate
+// Query pour portfolio public (visiteur)
+export const GET_PORTFOLIO_BY_USERNAME = gql`
+  query GetPortfolioByUsername($username: String!) {
+    getPortfolioByUsername(username: $username) {
+      user {
+        id
+        nom
+        prenom
+        username
+        bio
+      }
+      projects {
+        id
+        titre
+        description
+        urlGit
+        urlDemo
+        image
+      }
+      experiences {
+        id
+        company
+        role
+        startDate
+        endDate
+        description
+      }
+      formations {
+        id
+        filiere
+        ecole
+        localisation
+        dateDebut
+        dateFinal
+        description
+      }
+      skills {
+        id
+        name
+        categorie
+      }
+      documents {
+        _id
+        nom
+        urlStocket
+      }
+      social {
+        id
+        nom
+        liensSociaux
+        icon
+      }
     }
   }
 `;
